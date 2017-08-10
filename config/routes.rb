ab725f6e3e26af8bb1c3302df0e6466da5a48620
Edison Higuita <edihi2713@gmail.com>
2017-08-09 20:08:02 -0500

    correccion2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api , defaults: {format: "json"} do
+  namespace :api  do
     namespace :v1 do
-      resources :products
+      resources :products, defaults: {format: "json"} 
     end
   end
   
