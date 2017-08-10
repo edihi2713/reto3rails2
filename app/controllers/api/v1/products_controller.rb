bf77f2065f4226ef5bcb017abe2731c83d8d6bf3
Edison Higuita <edihi2713@gmail.com>
2017-08-09 19:59:17 -0500

    terminado

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: {message: "Guardado exitsamente"}, status: 201
+    else
+      render json:  { errors: @product.errors.full_messages } , status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.update(params[:id], product_params)
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
+end
